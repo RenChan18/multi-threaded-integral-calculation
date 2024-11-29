c55c4a1ecbd459f0af0d6d109b4d4a263d89d3c3
RenChan18 <renya.talacheva@gmail.com>
2024-11-29 11:27:00 +0300

    the implementation of the parallel algorithm has been added

--- include/parallel.h
+++ include/parallel.h
@@ -2,5 +2,24 @@
 
 #ifndef PARALLEL_H
 #define PARALLEL_H
+#include <stdio.h>
+#include <math.h>
+#include <pthread.h>
+#include <stdlib.h>
+#define NUM_THREADS 4
+
+typedef struct {
+    double (*func)(double);
+    double lower_limit;
+    double upper_limit;
+    int num_intervals;
+    int thread_id;
+    double partial_sum;
+} TD2;
+
+double calculate_integral_trapezoidal_segment(double (*func)(double), double lower_limit, double upper_limit, int num_intervals);
+void* thread_func_p(void* arg);
+
+void parallel(int initial_intervals, double (*func)(double));
 
 #endif // PARALLEL_H
--- src/parallel.c
+++ src/parallel.c
@@ -1,1 +1,58 @@
 // parallel.c
+#include "parallel.h"
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+double global_sum = 0.0;
+void* thread_func_p(void* arg) {
+    TD2* data = (TD2*)arg;
+
+    double interval_width = (data->upper_limit - data->lower_limit) / data->num_intervals;
+    int start = data->thread_id * (data->num_intervals / NUM_THREADS);
+    int end = start + (data->num_intervals / NUM_THREADS);
+
+    double local_sum = 0.0;
+    for (int i = start; i < end; i++) {
+        double x = data->lower_limit + i * interval_width;
+        local_sum += data->func(x);
+    }
+
+    pthread_mutex_lock(&mutex);
+    global_sum += local_sum;
+    pthread_mutex_unlock(&mutex);
+
+    pthread_exit(NULL);
+}
+
+double calculate_integral_parallel_sum(double (*func)(double), double lower_limit, double upper_limit, int num_intervals) {
+    pthread_t threads[NUM_THREADS];
+    TD2 thread_data[NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_data[i].func = func;
+        thread_data[i].lower_limit = lower_limit;
+        thread_data[i].upper_limit = upper_limit;
+        thread_data[i].num_intervals = num_intervals;
+        thread_data[i].thread_id = i;
+
+        pthread_create(&threads[i], NULL, thread_func_p, (void*)&thread_data[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    double interval_width = (upper_limit - lower_limit) / num_intervals;
+    double integral = (interval_width / 2) * (func(lower_limit) + func(upper_limit) + 2 * global_sum);
+    return integral;
+}
+
+void parallel(int initial_intervals, double (*func)(double)) {
+    double lower_limit, upper_limit;
+     printf("\nEnter the initial limit: ");
+    scanf("%lf", &lower_limit);
+    printf("\nEnter the final limit: ");
+    scanf("%lf", &upper_limit);
+    double integral = calculate_integral_parallel_sum(func, lower_limit, upper_limit, initial_intervals);
+    printf("The integral is: %lf\n", integral);
+
+}
+
