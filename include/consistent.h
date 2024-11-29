45ab571d892d2049cd6bedf133141360c6762db9
RenChan18 <renya.talacheva@gmail.com>
2024-11-29 11:25:54 +0300

    the implementation of the sequential algorithm has been changed

--- include/consistent.h
+++ include/consistent.h
@@ -2,10 +2,13 @@
 #ifndef CONSISTENT_H
 #define CONSISTENT_H
 
+//#include "read_data.h"
+#define TOLERANCE 1e-6
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 
+void read_input1(double* lower_limit, double* upper_limit);
 double calculate_integral_trapezoidal(double (*func)(double), double lower_limit, double upper_limit, int num_intervals);
 void perform_integration(int initial_intervals, double (*func)(double));
 
--- src/consistent.c
+++ src/consistent.c
@@ -1,6 +1,13 @@
 // consistent.c
 #include "consistent.h"
-#include <math.h>
+
+void read_input1(double* lower_limit, double* upper_limit)
+{
+    printf("\nEnter the initial limit: ");
+    scanf("%lf", lower_limit);
+    printf("\nEnter the final limit: ");
+    scanf("%lf", upper_limit);
+}
 
 double calculate_integral_trapezoidal(double (*func)(double), double lower_limit, double upper_limit, int num_intervals) {
     double interval_width = fabs(upper_limit - lower_limit) / num_intervals;
@@ -16,26 +23,20 @@ double calculate_integral_trapezoidal(double (*func)(double), double lower_limit
 
 void perform_integration(int initial_intervals, double (*func)(double)) {
     double lower_limit, upper_limit;
-    const double tolerance = 1e-6;
     int num_intervals = initial_intervals > 0 ? initial_intervals : 2;
-
-    printf("\nEnter the initial limit: ");
-    scanf("%lf", &lower_limit);
-    printf("\nEnter the final limit: ");
-    scanf("%lf", &upper_limit);
-
+    read_input1(&lower_limit, &upper_limit);
     double current_integral = calculate_integral_trapezoidal(func, lower_limit, upper_limit, num_intervals);
     double previous_integral;
 
     do {
         previous_integral = current_integral;
 
-        if (fabs(current_integral - previous_integral) >= tolerance || num_intervals == initial_intervals) {
+        if (fabs(current_integral - previous_integral) >= TOLERANCE || num_intervals == initial_intervals) {
             num_intervals++;
             current_integral = calculate_integral_trapezoidal(func, lower_limit, upper_limit, num_intervals);
         }
 
-    } while (fabs(current_integral - previous_integral) >= tolerance);
+    } while (fabs(current_integral - previous_integral) >= TOLERANCE);
 
     printf("The integral is: %lf\n", current_integral);
     printf("Calculated with %d intervals\n", num_intervals);
